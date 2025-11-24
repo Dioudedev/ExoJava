9326d32896e9b39836b1034be6f48cc10ea1630f
Dioudedev <diouldedev@gmail.com>
2025-11-24 15:05:14 +0000

    Create exo1.java

---
+++ exo1.java
@@ -0,0 +1,6 @@
+exo1:
+ligne06: second package → interdit
+ligne10:appel super() impossible (constructeur protected dans un autre package)
+ligne12:accès illégal à i (package différent)
+ligne13:méthode redéfinie en private alors qu’elle doit être public
+ligne17:troisième déclaration de package → interdit
